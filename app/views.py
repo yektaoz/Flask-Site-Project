758e32658f996897c79003e85ab6301c7d8149c7
yektaoz <tqery123@gmail.com>
2024-01-27 19:14:58 +0300

    0.1

---
+++ app/views.py
@@ -0,0 +1,47 @@
+from flask import render_template, redirect, url_for, request, flash
+from app import app, db
+from app.models import Movie
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+@app.route('/testimonial')
+def testimonial():
+    return render_template('testimonial.html')
+
+
+@app.route("/purchase/<int:id>")
+def purchase_movie(id):
+    movie = Movie.query.get_or_404(id)
+    return render_template("purchase.html")
+
+@app.route("/cart")
+def cart(id):
+    pass
+
+
+
+
+
+  
+
+  
+
+
